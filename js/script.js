316f95dd112c3c29377f9e534d86ea8f7e710da3
Tristan Young <tristan.young@verizon.com>
2019-06-21 13:50:51 -0500

    update

--- js/script.js
+++ js/script.js
@@ -7,27 +7,21 @@ FSJS project 2 - List Filter and Pagination
 //Create Global Variable with a value of student list
 const studentList = document.getElementsByClassName('student-item');
 const itemsPerPage = 10;
-console.log(studentList);
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//console.log(studentList);
+
    function getNumberOfPages() {
       return Math.ceil(studentList.length / itemsPerPage);
    }
+/*** 
+   Create showPage function below
+      create two vaiables for start and end index
+      this functions only allows 10 items to show per page
+         - insert a for loop to only display the pages you want created by the previous 2 variables
+         - if i is greater than or equal to [0]1 and i is less than 10 then display student list 
+***/
 
 
-//this functions only allows 10 items to show.
-function showPage(studentList, page){
+function showPage(list, page){
    let startIndex = (page * itemsPerPage) - itemsPerPage;
    let endIndex = page * itemsPerPage;
 
@@ -42,38 +36,47 @@ function showPage(studentList, page){
 
 /*** 
 call the showPage function to verify only 10 items show
-showPage(studentList, 5);
+showPage(studentList, 1);
 ***/
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   Create the append page function with a parameter of total items
+
 ***/
 
 function appendPageLinks(studentList) {
    let numberPages = getNumberOfPages
    //create div element dynamically
+   let page = document.querySelector('.page')
    let div = document.createElement('div');
-   div.className =('pagination');
    let ul = document.createElement('ul')
-   document.querySelector('.page').appendChild(div);
+   div.className =('pagination');
+   div.setAttribute('class', 'pagination');
+   page.appendChild(div);
    div.appendChild(ul);
    
 
-for(let i = 1; i <= getNumberOfPages(); i++) {
+for(let i = 0; i < getNumberOfPages(); i++) {
     let li = document.createElement('li');
     let a = document.createElement('a');
     ul.appendChild(li);
     li.appendChild(a);
-    a.innerHTML = i;
-    a.addEventListener('click', e => {
-         e.target.className = "A";
+    a.textContent = i + 1;
+    a.addEventListener('click', (event) => {
+         let a = document.querySelectorAll('.pagination li a');
+         for(let b = 0; b < a.length; b++ ){
+            a[b].className = '';
+         }
+            event.target.className = 'acvtive';
+            showPage(studentList, event.target.textContent);
          
     });
   }
 }
-//showPage(studentList, 1);
+
+//showPage(studentList, textContent);
+showPage(studentList, 1);
 appendPageLinks();
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
 
